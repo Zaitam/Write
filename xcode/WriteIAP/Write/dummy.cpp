a5524088c0e5802e01fba194901a933aa08037ac
pbsurf <>
2025-04-28 09:53:33 -0700

    add xcode projects

---
+++ xcode/WriteIAP/Write/dummy.cpp
@@ -0,0 +1,9 @@
+//
+//  dummy.c
+//  Write
+//
+//  Created by mwhite on 3/3/20.
+//  Copyright © 2020 mwhite. All rights reserved.
+//
+
+bool xcode_build_dummy = true;
---
+++ xcode/WriteIAP/Write/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Write
+//
+//  Created by mwhite on 1/5/20.
+//  Copyright © 2020 mwhite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  NSString * appDelegateClassName;
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+      appDelegateClassName = NSStringFromClass([AppDelegate class]);
+  }
+  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
